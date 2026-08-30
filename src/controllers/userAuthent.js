3a4ef0680d6137e2d006a148a7dba37afb6b7588
Sumit Singh <sumitsingh125best@gmail.com>
2026-08-30 18:29:39 +0530

    Implement user authentication

--- src/controllers/userAuthent.js
+++ src/controllers/userAuthent.js
@@ -1,8 +1,10 @@
 const User = require("../models/user");
-
 const validate = require("../utils/validator");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
-const register = (req, res) =>{
+
+const register = async (req, res) =>{
 
     try{
 
@@ -10,17 +12,61 @@ const register = (req, res) =>{
         validate(req.body);
 
 
-        const {firstName, emailID, password} = req.body;
+        const {firstName, emailId, password} = req.body;
 
-        User.create(req.body);
+        // hash the password
+        req.body.password = await bcrypt.hash(password, 10);
 
+        const user = await User.create(req.body);  // it will check emailId already exist or not so no need to do
+
+        const token = jwt.sign({_id: user._id, emailID: emailId},process.env.JWT_KEY, {expiresIn: 60*60});
+        res.cookie('token', token,{maxAge: 60*60*1000});  // 1 hours
+        res.status(201).send("User Registered Successfully");
     }
 
     catch(err){
+        res.status(400).send("Error: "+err);
+    }
 
+}
+
+const login = async (req, res) =>{
+
+    try{
+
+        const {emailId, password} = req.body;
+
+        if(!emailId)
+            throw new Error("Invalid EmialId");
+
+        if(!password)
+            throw new Error("Invalid credentials");
+
+        const user = await User.findOne({emailId});
+        const match = await bcrypt.compare(password, user.password);
+
+        if(!match)
+            throw new Error("Invalid credentials");
+
+        const token = jwt.sign({_id: user._id, emailID: emailId},process.env.JWT_KEY, {expiresIn: 60*60});
+        res.cookie('token', token,{maxAge: 60*60*1000});
+        res.status(200).send("Logged In Successfully");
+    }
+
+    catch(err){
+        res.status(401).send("Error: "+err);
+    }
+}
+
+const logout = async (req, res) =>{
+
+    try{
 
     }
 
+    catch{
+
+    }
 
 
 }
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -8,6 +8,17 @@ const validate = (data)=>{
 
     const mandatoryFeild = ["firstName", "emialID", "password"];
     const IsAllowed = mandatoryFeild.every((k)=> Object.keys(data).includes(k));
+
+    if(!IsAllowed)
+        throw new Error("Some feild missing");
+
+    if(!validator.isEmail(data.emailId))
+        throw new Error("Invalid Email");
+
+    if(!validator.isStrongPassword(data.password))
+        throw new Error("Week Password");
+
+    
 }
 
 module.exports = validate;
